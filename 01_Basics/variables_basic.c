66f2ce623be9a38615593d2af7b85140054d446f
Bablu Kushwaha <bablu8602@xavier.edu.np>
2025-12-27 09:27:00 +0545

    Add variables code

--- 01_Basics/variables_basic.c
+++ 01_Basics/variables_basic.c
@@ -1,1 +1,11 @@
+// This program demonstrates basic variable declaration and usage
 
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    
+    printf("My age is: %d\n", age);
+    
+    return 0;
+}
