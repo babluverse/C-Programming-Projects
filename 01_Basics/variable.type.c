ede2fa9114a0dbc27312394b28765a7e6e1bd520
Bablu Kushwaha <bablu8602@xavier.edu.np>
2025-12-28 21:20:35 +0545

    Add data type.

--- 01_Basics/variable.type.c
+++ 01_Basics/variable.type.c
@@ -1,1 +1,17 @@
+// Program to show different data types in C
 
+#include <stdio.h>
+
+int main() {
+    int number = 10;
+    float decimal = 3.14;
+    char letter = 'A';
+    double big_decimal = 99.99999;
+    
+    printf("Integer: %d\n", number);
+    printf("Float: %.2f\n", decimal);
+    printf("Character: %c\n", letter);
+    printf("Double: %.5lf\n", big_decimal);
+    
+    return 0;
+}
