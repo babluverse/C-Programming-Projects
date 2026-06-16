62915707d5a8d575cf721326d669cf59b5a8a65e
babluverse <bablu8602@xavier.edu.np>
2026-06-16 18:27:46 +0545

    Add 05_multiple_variables

--- 01_Basics/05_multiple_variables.c
+++ 01_Basics/05_multiple_variables.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int age = 20;
+    float height = 5.9;
+    char grade = 'A';
+    double salary = 25000.50;
+
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Grade: %c\n", grade);
+    printf("Salary: %.2lf\n", salary);
+
+    return 0;
+}
